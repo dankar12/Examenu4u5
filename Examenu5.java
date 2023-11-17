6c96cda4bc754970a08b1dc90f5a7fc6a7c707c7
dankar12 <143622228+dankar12@users.noreply.github.com>
2023-11-17 08:51:57 -0600

    Add files via upload

---
+++ Examenu5.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenu5;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examenu5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double[][] ventas = {
+        {1500.0, 2200.0, 1800.0, 2000.0},
+        {1200.0, 2800.0, 2100,0, 1900.0},
+        {1600.0, 1900.0, 2200.0, 2300.0}
+    };
+    //llamada al primer metodo
+    double promedioMensual=
+            calcularPromedioMensual(ventas);
+    System.out.println)("Promedio de ventas mensual.)
+            
+    
+    
+    
+    
+    }
+    }
+    
+
